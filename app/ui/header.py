024e1c95db7024dc5da2f6f1365c540ba30662b7
Marco De Stefano <destmark@gmail.com>
2025-08-21 10:04:55 +0000

    fixed link in header

--- app/ui/header.py
+++ app/ui/header.py
@@ -3,16 +3,14 @@ from app.services import pages
 from app.ui import styles
 
 def render():
-    #ui.add_head_html("""
-#<script>document.documentElement.setAttribute("data-theme",'"""+app.storage.user.get('theme', 'light')+"""');</script>""")
     with ui.left_drawer(elevated=True, value=False).classes('bg-base-100') as left_drawer:
         with ui.element('ul').classes('menu w-full'):
             with ui.element('li'):
-                with ui.element('a').props('href='+pages.HOME_PAGE):
+                with ui.element('a').on('click', lambda: ui.navigate.to(pages.HOME_PAGE)):
                     ui.html(styles.HOME_SVG)
                     ui.label('Dashboard')
             with ui.element('li'):
-                with ui.element('a').props('href='+pages.EXPENSES_PAGE):
+                with ui.element('a').on('click', lambda: ui.navigate.to(pages.EXPENSES_PAGE)):
                     ui.html(styles.EXPENSES_SVG)
                     ui.label('Expenses')
     
