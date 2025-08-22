fdc2b46d32d20e3699227ae1117cca87ae367004
Marco De Stefano <destmark@gmail.com>
2025-08-22 15:58:07 +0000

    update dataframe creation

--- app/services/google_sheets.py
+++ app/services/google_sheets.py
@@ -1,8 +1,7 @@
-# app/services/google_sheets.py
 import gspread
 import pandas as pd
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 class GoogleSheetService:
     def __init__(self, credentials_path: Union[str, Path], workbook_id: str):
@@ -13,16 +12,22 @@ class GoogleSheetService:
         self.client = self._authenticate()
 
     def _authenticate(self):
-        """Authenticates with Google Sheets using service account."""
         return gspread.service_account(filename=str(self.creds_path))
 
-    def get_worksheet_as_dataframe(self, worksheet_name: str) -> pd.DataFrame:
-        """Fetches a specific worksheet and returns it as a pandas DataFrame."""
+    def get_worksheet_as_dataframe(self, worksheet_name: str, header: Union[int, List[int]] = 0, index_col: Union[int, None] = 0) -> pd.DataFrame:
         try:
             sheet = self.client.open_by_key(self.workbook_id).worksheet(worksheet_name)
-            # Get all records from the sheet and convert to DataFrame
-            data = sheet.get_all_records()
-            return pd.DataFrame(data)
+            data = sheet.get_all_values()
+            if not data:
+                return pd.DataFrame()
+            df = pd.DataFrame(data)
+            df.columns = df.iloc[header]
+            header_rows_to_drop = [header] if isinstance(header, int) else header
+            df = df.drop(header_rows_to_drop)
+            if index_col is not None:
+                index_name = df.columns[index_col]
+                df = df.set_index(index_name)
+            return df.reset_index()
         except gspread.exceptions.WorksheetNotFound:
-            print(f"Error: Worksheet '{worksheet_name}' not found in '{self.workbook_id}'.")
-            return pd.DataFrame() # Return empty dataframe on error
+            print(f"Error: Worksheet '{worksheet_name}' not found.")
+            return pd.DataFrame()
